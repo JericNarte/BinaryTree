94f4be7c39614d87ea638918777792c6c2b44b01
Jeric <jericnarte912@gmail.com>
2021-05-23 04:13:05 +0800

    v1.2
    Finished MyBinaryTree class

--- src/MyBinaryTree.java
+++ src/MyBinaryTree.java
@@ -1,4 +1,5 @@
 public class MyBinaryTree {
+    // Binary Tree Node
     private class BTNode {
         char data;
         BTNode left;
@@ -11,7 +12,7 @@ public class MyBinaryTree {
         }
     }
 
-    private final MyLinkedList<Character> arr;
+    private MyLinkedList<Character> arr;
     private BTNode root;
     private int nodeCount;
 
@@ -21,13 +22,14 @@ public class MyBinaryTree {
         nodeCount = 0;
     }
 
+    // Insert a values into the Binary Tree in order level
     public void insert(char data) {
         arr.add(data);
         nodeCount++;
         root = insertNode(arr, root, 0);
-
     }
 
+    // Insert an array of values into the Binary Tree in order level
     public void insert(char[] data) {
         for (char v : data) {
             arr.add(v);
@@ -36,6 +38,7 @@ public class MyBinaryTree {
         root = insertNode(arr, root, 0);
     }
 
+    // Create a Binary Tree base on the values of the array
     private BTNode insertNode(MyLinkedList<Character> arr, BTNode root, int i) {
         if (i < arr.length()) {
             root = new BTNode(arr.get(i));
@@ -45,19 +48,25 @@ public class MyBinaryTree {
         return root;
     }
 
+    // Delete the existing Binary Tree
+    public void emptyTree() {
+        arr = new MyLinkedList<>();
+        root = null;
+        nodeCount = 0;
+        System.out.println("> The Binary Tree is deleted");
+    }
+
+    // Get the number of Nodes or size of the Binary Tree
     public int countNodes() {
         return nodeCount;
     }
 
-    public void emptyTree() {
-        root = null;
-        System.out.println("> The Binary Tree is deleted");
-    }
-
+    // Get the height of the Binary Tree
     public int treeHeight() {
         return treeHeightRecursion(root);
     }
 
+    // Recursive function to get the height of the Binary Tree
     public int treeHeightRecursion(BTNode current) {
         if (current == null) {
             return 0;
@@ -66,6 +75,7 @@ public class MyBinaryTree {
         return ++left;
     }
 
+    // Print the elements inside the Binary Tree in Post-Order
     public void postOrder() {
         if (nodeCount == 0) {
             System.out.println("> The Binary Tree is Empty.");
@@ -73,26 +83,6 @@ public class MyBinaryTree {
         }
         System.out.print("Post-Order: ");
         printPostOrder(root);
-        System.out.println();
-    }
-
-    public void preOrder() {
-        if (nodeCount == 0) {
-            System.out.println("> The Binary Tree is Empty.");
-            return;
-        }
-        System.out.print("Pre-Order: ");
-        printPreOrder(root);
-        System.out.println();
-    }
-
-    public void inOrder() {
-        if (nodeCount == 0) {
-            System.out.println("> The Binary Tree is Empty.");
-            return;
-        }
-        System.out.print("In-Order: ");
-        printInOrder(root);
         System.out.println();
     }
 
@@ -104,6 +94,17 @@ public class MyBinaryTree {
         }
     }
 
+    // Print the elements inside the Binary Tree in Pre-Order
+    public void preOrder() {
+        if (nodeCount == 0) {
+            System.out.println("> The Binary Tree is Empty.");
+            return;
+        }
+        System.out.print("Pre-Order: ");
+        printPreOrder(root);
+        System.out.println();
+    }
+
     private void printPreOrder(BTNode current) {
         if (current != null) {
             System.out.print(current.data + "  ");
@@ -111,6 +112,17 @@ public class MyBinaryTree {
             printPreOrder(current.right);
         }
 
+    }
+
+    // Print the elements inside the Binary Tree in In-Order
+    public void inOrder() {
+        if (nodeCount == 0) {
+            System.out.println("> The Binary Tree is Empty.");
+            return;
+        }
+        System.out.print("In-Order: ");
+        printInOrder(root);
+        System.out.println();
     }
 
     private void printInOrder(BTNode current) {
@@ -121,58 +133,62 @@ public class MyBinaryTree {
         }
     }
 
+    // Print the Binary Tree Model
     public void PrintTree() {
         if (nodeCount == 0) {
             System.out.println("> The Binary Tree is Empty.");
             return;
         }
-        print(arr);
-    }
 
-    private void print(MyLinkedList<Character> list) {
         int counter = 0;
         int treeHeight = treeHeight();
 
+        // Each loop prints a level of the Binary Tree
         for (int i = 0; i < treeHeight; i++) {
             int space = (int) Math.pow(2, (treeHeight - i)) / 2;
             int gap = (int) Math.pow(2, (treeHeight - i)) - 1;
-            int slots = (int) Math.pow(2, i);
 
-            for (int j = 0; j < slots; j++) {
+            // Print values of the (i)th level of the Binary Tree
+            for (int j = 0; j < Math.pow(2, i); j++) {
                 printSpace((j == 0) ? space : gap);
-                System.out.print(" " + list.get(counter++) + " ");
-                if (counter >= list.length()) {
-                    break;
-                }
+                System.out.print(" " + arr.get(counter++) + " ");
+                if (counter >= arr.length()) break;
             }
-            if (i == treeHeight - 1) { break; }
 
+            // No need to print connecting lines for Leaf Nodes
+            if (i == treeHeight - 1) break;
+
+            // Print a series of connecting dots to each child of a node in the (i)th level of the Binary Tree
             for (int tempSpace = space - 1; tempSpace >= space / 2; tempSpace--) {
-                System.out.println();
-                printSpace(tempSpace);
                 int flag = 0;
+                int gapIN = ((space - tempSpace) * 2) - 1;
+                int gapOUT = ((tempSpace) * 2) - 1;
+
+                // Print a dot toward each children nodes
+                System.out.println();
                 for (int j = 0; j < Math.pow(2, i + 1); j++) {
+                    printSpace((j == 0) ? tempSpace : (counter % 2 == 0) ? gapIN : gapOUT);
                     System.out.print(" . ");
-                    int gapIN = ((space - tempSpace) * 2) - 1;
-                    int gapOUT = ((tempSpace) * 2) - 1;
-                    printSpace((counter % 2 == 1) ? gapIN : gapOUT);
                     counter++;
                     flag++;
-                    if (list.length() <= counter) {
-                        break;
-                    }
+                    if (arr.length() <= counter) break;
                 }
-                counter -= flag;
+
+                // Control the height between parent and child nodes
                 tempSpace -= Math.floor((float) space * 0.1);
+                counter -= flag;
             }
             System.out.println();
         }
     }
-
 
     private void printSpace(int count) {
         for (int i = 0; i < count; i++) {
             System.out.print("   ");
         }
     }
+
+
+
+
 }
